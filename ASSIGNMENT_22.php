9c6e66716029ace670ab47e04837104ed05f8930
Rashi Dashore <dash.rashi07@gmail.com>
2023-07-05 00:42:15 +0530

    Initial commit

---
+++ ASSIGNMENT_22.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+date_default_timezone_set('Asia/Kolkata');
+$date = date('d-m-y h:i:s');
+echo $date;
+
+?>
+<hr>
+<p>By Rashi Dashore.</p>
+<p> Roll No. : IC-2K20-67 </p>
+Thankyou
+<hr>
+
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,2 @@
+# WebDevelopment-projects
+ These are the web development projects I did in my college
